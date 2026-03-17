50f53f96f5b901fbd32ae854aac373f924da04cf
okubo-jo <jtk1028@icloud.com>
2026-03-17 13:24:07 +0900

    最初のコミット

---
+++ calculator app.py
@@ -0,0 +1,10 @@
+print("=== 簡単な計算機アプリ ===")
+
+a = float(input("1つ目の数字を入力: "))
+b = float(input("2つ目の数字を入力: "))
+
+print("足し算:", a + b)
+print("引き算:", a - b)
+print("掛け算:", a * b)
+print("割り算:", a / b)
+
